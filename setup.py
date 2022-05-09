772fd9a753c7335ff13f70bebb747338556bde42
DolgihPavel <dolgihgod@mail.ru>
2022-05-09 20:21:35 +0300

    added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+
+setup(
+name="serializers",
+version="1.0",
+author="pasha",
+author_email="pashadol02@gmail.com",
+description="json, toml, yaml serializers",
+keywords="json toml yaml serializer",
+url="https://github.com/DolgihPavel/ISP-2022-053504/tree/dev-task-2"
+)
